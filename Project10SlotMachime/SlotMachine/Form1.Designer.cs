2d34ec169f587de1f263ff04943a1692016d22a1
James Calvin <calvija@quincy.edu>
2019-01-16 18:13:11 -0600

    Add files via upload
    This is a Slot Machine recreated in Visual Studio 2017, this is an updated version of my original assignment in my Intro to Software Development Class it is written in C#

---
+++ Project10SlotMachime/SlotMachine/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace SlotMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.Spin = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Amount = new System.Windows.Forms.Label();
+            this.Amount_Box = new System.Windows.Forms.TextBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Stop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(10, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 109);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(116, 9);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 108);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(222, 9);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 108);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Spin
+            // 
+            this.Spin.Location = new System.Drawing.Point(73, 190);
+            this.Spin.Name = "Spin";
+            this.Spin.Size = new System.Drawing.Size(75, 23);
+            this.Spin.TabIndex = 3;
+            this.Spin.Text = "Spin";
+            this.Spin.UseVisualStyleBackColor = true;
+            this.Spin.Click += new System.EventHandler(this.Spin_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Location = new System.Drawing.Point(116, 232);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(75, 23);
+            this.Exit.TabIndex = 4;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Amount
+            // 
+            this.Amount.AutoSize = true;
+            this.Amount.Location = new System.Drawing.Point(48, 149);
+            this.Amount.Name = "Amount";
+            this.Amount.Size = new System.Drawing.Size(87, 13);
+            this.Amount.TabIndex = 5;
+            this.Amount.Text = "Amount Inserted:";
+            // 
+            // Amount_Box
+            // 
+            this.Amount_Box.Location = new System.Drawing.Point(141, 146);
+            this.Amount_Box.Name = "Amount_Box";
+            this.Amount_Box.Size = new System.Drawing.Size(100, 20);
+            this.Amount_Box.TabIndex = 6;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(247, 123);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(247, 149);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox5.TabIndex = 8;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Visible = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(247, 205);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox6.TabIndex = 9;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "label2";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(166, 190);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 12;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 261);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.Amount_Box);
+            this.Controls.Add(this.Amount);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Spin);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button Spin;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Label Amount;
+        private System.Windows.Forms.TextBox Amount_Box;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Stop;
+    }
+}
+
---
+++ Project10SlotMachime/SlotMachine/Form1.cs
@@ -0,0 +1,182 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace SlotMachine
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int SlowTime;
+        int pic1;
+        int pic2;
+        int pic3;
+
+        decimal Winings = 0;
+        decimal TotalBets = 0;
+        decimal BetPlaced;
+
+        private void Spin_Click(object sender, EventArgs e)
+        {
+
+            //displays pictures in picture box randomly
+
+            timer1.Interval = 100;
+            timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Image[] images = new Image[4];
+
+            images[0] = pictureBox4.Image;
+            images[1] = pictureBox5.Image;
+            images[2] = pictureBox6.Image;
+
+
+
+
+            /*SlowTime = DateTime.Now.Second;
+            SlowTime += 1;
+            SlowTime %= 60;
+            while (DateTime.Now.Second <= SlowTime)
+            { }
+            */
+
+
+            /*
+			
+			You only have one random cycling through your pictures.
+			If you think about it if the mix_pic chooses 1, then
+			all of the images become 1.
+			
+			In order to fix this you need to create 2 more others
+			to treat the other 2 pictures randomly.
+			
+			mix_pic will change the first picture,
+			mix_pic_2 will change the second picture,
+			mix_pic_3 will change the third picture.
+				
+			*/
+            Random mix_pic = new Random();
+
+            // Added mix_pic_2 and mix_pic_3_3
+            Random mix_pic_2 = new Random();
+            Random mix_pic_3 = new Random();
+
+            pictureBox1.Image = images[mix_pic.Next(0, 3)];
+
+            // Added
+            pictureBox2.Image = images[mix_pic_2.Next(1, 3)];
+            pictureBox3.Image = images[mix_pic_3.Next(0, 4)];
+
+            // Added
+            int pic1 = mix_pic.Next(0, 3);
+            int pic2 = mix_pic_2.Next(0,3);
+            int pic3 = mix_pic_3.Next(0,3);
+
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            decimal CurrentWinings = 0;
+
+            decimal.TryParse(Amount_Box.Text, out BetPlaced);
+
+            /*
+			int StopTime = DateTime.Now.Second;
+			label1.Text = StopTime.ToString();
+			StopTime += 5;
+			StopTime %= 60;
+			label2.Text = StopTime.ToString();
+			*/
+
+            //based on random 1-3; compare the 3 numbers example: pic1 = 1, pic2 = 1
+            if (pictureBox1.Image != pictureBox2.Image && pictureBox2.Image != pictureBox3.Image)
+            {
+
+
+                Winings = Winings + 0;
+                BetPlaced = BetPlaced + 0;
+                TotalBets = BetPlaced + 0;
+
+                CurrentWinings = TotalBets + 0;
+
+                MessageBox.Show(" You Haven won: " +CurrentWinings+ " Better luck next time !");              //check math
+            }
+
+            /*
+			
+			You have to satisfy all possible outcomes for the first two outcomes.
+			
+			You got if pic1 is pic2 and pic2 is not pic3.
+			Don't forget the second outcome, what if pic2 IS pic3?
+			
+			The player wins when pic1 is pic3 and pic1 is not pic2.
+			The player wins when pic2 is pic3 and pic1 is not pic3.
+			
+			*/
+            else if (pictureBox1.Image == pictureBox2.Image && pictureBox2.Image != pictureBox3.Image || pictureBox2.Image == pictureBox3.Image && pictureBox1.Image != pictureBox3.Image)
+            {
+                Winings = Winings + (BetPlaced * 2);
+
+                TotalBets = TotalBets + BetPlaced;
+
+                CurrentWinings = CurrentWinings + Winings;
+
+                MessageBox.Show("Congrats! You have Won: " + CurrentWinings + " You Know you want to play again..");
+            }
+
+            else if (pictureBox1.Image == pictureBox2.Image && pictureBox2.Image == pictureBox3.Image && pictureBox1.Image==pictureBox3.Image)
+            {
+                Winings = Winings + (BetPlaced * 3);
+
+                TotalBets = TotalBets + BetPlaced;
+
+                CurrentWinings = CurrentWinings + Winings;
+
+                MessageBox.Show("What?! You hit the JACKPOT! YOU HAVE WON: " + CurrentWinings);
+            }
+
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+
+            string Message = "Thanks! You have given ME: " + TotalBets.ToString("c")
+                + "\n" + "I guess you have can keep the money won from me" + Winings.ToString("c");
+
+            DialogResult TheMessege = MessageBox.Show(Message, "Thanks for Playing", MessageBoxButtons.OK);
+
+            if (TheMessege == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+
+        }
+
+    }
+
+}
